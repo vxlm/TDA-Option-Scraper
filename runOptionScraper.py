6d743cedd1bfdf5b1b8992bb8a4cb05acedf627c
Victoria <v13472889991@gmail.com>
2021-06-13 11:51:54 -0400

    Update runOptionScraper.py

--- runOptionScraper.py
+++ runOptionScraper.py
@@ -2,7 +2,7 @@ import pandas as pd
 import os
 df = pd.read_csv('./nasdaq_screener_1623597138806.csv')
 print(df)
-for index,row in df.head().iterrows():
+for index,row in df.iterrows():
     print(row['Symbol'])
     os.system('node optionsData.js ' +str(row['Symbol']))
     print('finished')
