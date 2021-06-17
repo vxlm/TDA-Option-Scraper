3e01ae644f64f0a96d421c5367b42122f456c46d
Victoria <vxl@umich.edu>
2021-06-17 10:41:16 -0400

    Concurrency for faster scraping
    Optimized function for using multiprocessing to run multiple processes, in order to be able to scrape the maximum amount of tickers a minute without being rate limited.

--- runOptionScraper.py
+++ runOptionScraper.py
@@ -1,11 +1,38 @@
 import pandas as pd
 import os
-df = pd.read_csv('./nasdaq_screener_1623597138806.csv')
-print(df)
-for index,row in df.iterrows():
-    print(row['Symbol'])
-    os.system('node optionsData.js ' +str(row['Symbol']))
-    print('finished')
-    
-        
+import datetime
+import multiprocessing
+from multiprocessing import Pool
+
+
+#Old scrape function, which is much less efficient for multiprocessing than scrape_ticker. Takes in as input a whole dataframe
+#with a column symbols to scrape.
+# def scrape_data(df):
+#     for index,row in df.iterrows():
+#         endTime = datetime.datetime.now() + datetime.timedelta(seconds=1)
+#         print(row['Symbol'])
+#         os.system('node optionsData.js ' +str(row['Symbol']))
+#         while datetime.datetime.now() <= endTime:
+#             pass
+#         print('done with ' + row['Symbol'])
+
+def scrape_ticker(ticker):
+    """
+    Scrape ticker will take in a ticker, and start scraping data into mongodb. It makes sure that each function call takes
+    at least one second, this guarantees that you can run 20 pool instances of it without rate limiting yourself on the TDA api.
+    """
+    endTime = datetime.datetime.now() + datetime.timedelta(seconds=1)
+    print(ticker)
+    os.system('node optionsData.js ' +str(ticker))
+    while datetime.datetime.now() <= endTime:
+        pass
+    print('done with ' + ticker)
+            
+if __name__ == '__main__':
+    #Change this csv for a custom symbol collection
+    df = pd.read_csv('./nasdaq_screener_1623597138806.csv')
+    #Modify this for however many concurrent scrapers you want going off at once, but remember you cannot exceed 20. 
+    workers = 2
+    with Pool(workers) as p:
+         results = p.map(scrape_ticker, df['Symbol'].tolist())
 
